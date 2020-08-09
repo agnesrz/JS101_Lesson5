02714ca5d14b3d8e8e321046f6acc65b2412a0ee
Agnes Rzepecki <aga.r909@gmail.com>
2020-08-09 01:15:34 +0000

    adding coursework

--- practice_problems.js
+++ practice_problems.js
@@ -61,8 +61,145 @@ let maleTotalAge = 0;
 
 Object.keys(munsters).forEach(familyMember => {
   if (munsters[familyMember].gender === 'male') {
-    maletotalAge += munsters[familyMember].age; 
+    maleTotalAge += munsters[familyMember].age; 
   }
 });
 
 console.log(maleTotalAge);
+
+// Problem 6
+let munsters = {
+  herman: { age: 32, gender: 'male' },
+  lily: { age: 30, gender: 'female' },
+  grandpa: { age: 402, gender: 'male' },
+  eddie: { age: 10, gender: 'male' },
+  marilyn: { age: 23, gender: 'female'}
+};
+
+Object.keys(munsters).forEach(member => {
+  let age = munsters[member].age;
+  let gender = munsters[member].gender;
+  
+  console.log(`${member} is an ${age}-year old ${gender}.`);
+});
+
+// Problem 8
+let obj = {
+  first: ['the', 'quick'],
+  second: ['brown', 'fox'],
+  third: ['jumped'],
+  fourth: ['over', 'the', 'lazy', 'dog'],
+};
+
+const VOWELS = ['a', 'e', 'i', 'o', 'u'];
+
+Object.values(obj).forEach(array => {
+  array.forEach(word => {
+    word.split('').forEach(letter => {
+      if (VOWELS.includes(letter)) {
+        console.log(letter);
+      }
+    });   
+  });
+})
+
+// Problem 9
+let arr = [['b', 'c', 'a'], [2, 1, 3], ['blue', 'black', 'green']];
+let sortedArray = JSON.parse(JSON.stringify((arr)));
+
+sortedArray.forEach(array => {
+  if (typeof array[0] === 'string') {
+    array.sort();
+  } else {
+    array.sort((a, b) => a - b);
+  }
+});
+
+console.log(sortedArray);
+
+// Problem 10
+let arr = [['b', 'c', 'a'], [2, 1, 3], ['blue', 'black', 'green']];
+
+console.log(arr.map(subArray => {
+  if (typeof subArray[0] === 'string') {
+    return subArray.slice().sort((a, b) => {
+      if (a < b) {
+        return 1;
+      } else if (a > b) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+  } else {
+    return subArray.slice().sort((a, b) => b - a);
+  }
+}));
+
+// Problem 11
+let arr = [{ a: 1 }, { b: 2, c: 3 }, { d: 4, e: 5, f: 6 }];
+
+let newArr = arr.map(obj => {
+  let updatedObj = Object.assign({}, obj);
+  for (let key in updatedObj) {
+    updatedObj[key] += 1;
+  }
+  return updatedObj;
+});
+
+console.log(newArr);
+
+// Problem 12
+let arr = [[2], [3, 5, 7], [9], [11, 15, 18]];
+
+let newArr = arr.map(array => {
+  return array.filter(value => value % 3 === 0);
+});
+
+console.log(newArr);
+
+// Problem 13
+let arr = [[1, 6, 7], [1, 5, 3], [1, 8, 3]];
+
+let sumOfOddNumsReducer = ((acc, currVal) => {
+  if (currVal % 2 !== 0) {
+    return acc + currVal;
+  } else {
+    return acc;
+  }
+});
+
+arr.sort((a, b) => {
+  let sumOfOddNumsA = a.reduce(sumOfOddNumsReducer, 0);
+  let sumOfOddNumsB = b.reduce(sumOfOddNumsReducer, 0);
+
+  return sumOfOddNumsA - sumOfOddNumsB;
+});
+
+console.log(arr);
+
+// Problem 14
+let obj = {
+  grape: { type: 'fruit', colors: ['red', 'green'], size: 'small' },
+  carrot: { type: 'vegetable', colors: ['orange'], size: 'medium' },
+  apple: { type: 'fruit', colors: ['red', 'green'], size: 'medium' },
+  apricot: { type: 'fruit', colors: ['orange'], size: 'medium' },
+  marrow: { type: 'vegetable', colors: ['green'], size: 'large' },
+};
+
+let fruitVegArray = [];
+
+for (let key in obj) {
+  if (obj[key].type === 'fruit') {
+    fruitVegArray.push(obj[key].colors.map(color => {
+      return color.slice(0, 1).toUpperCase() +
+             color.slice(1);
+    }));
+  } else {
+    fruitVegArray.push(obj[key].size.toUpperCase());
+  }
+}
+
+console.log(fruitVegArray);
+
+// Problem 15
